f1532bf9a778eeac0b456cf6ddca1f861ce68354
Trudovk <Trydovk@yandex.ru>
2023-02-21 11:25:36 +0300

    add index

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Кошелев Егор</title>
+    <style>
+        :root {
+            background-color: #333;
+            color: #eee;
+            text-align: center;
+            font-family: sans-serif;
+        }
+
+        ul {
+            text-align: left;
+            max-width: 40em;
+            margin: 1em auto;
+        }
+
+        a {
+            color: #f8d;
+        }
+
+        :visited {
+            color: #f8d;
+        }
+
+        header {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            background-color: rgb(30, 30, 30);
+            border-radius: 20px;
+            margin: 5px;
+            padding: 5px 10px;
+        }
+
+        body {
+            margin: 0;
+        }
+    </style>
+</head>
+
+<body>
+    <header>
+        <img src="/images/wpolylogo.png" alt="logo">
+        <h1>Кошелев Егор 221-323</h1>
+    </header>
+    <main>
+        <h2>Лабораторные работы</h2>
+        <ul>
+            <li>
+                <a href="laba2-1.php">
+                    Лабораторная работа 2.1 "Hello, word!"
+                </a>
+            </li>
+        </ul>
+    </main>
+</body>
+
+</html>
